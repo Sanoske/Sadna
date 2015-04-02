8741c130302bfdec97a0c5bdd0c5f0d88d202171
guy120494 <guy120494@gmail.com>
2015-04-03 00:37:35 +0300

    functions moved to AlgorithmUtils

--- src/Forest.java
+++ src/Forest.java
@@ -2,20 +2,32 @@ import java.util.*;
 
 public class Forest {
 	private List<ClusteringTree> list = null;
+	private int numberOfTrees;
 	
 	public Forest () {
-		list = new ArrayList<ClusteringTree>();
+		this.list = new ArrayList<ClusteringTree>();
+		this.numberOfTrees = 0;
 	}
 	
 	public void addTree(ClusteringTree t) {
 		list.add(t);
+		this.numberOfTrees++;
 	}
 	
 	public void removeTree(ClusteringTree t) {
 		list.remove(t);
+		this.numberOfTrees--;
 	}
 	
 	public ClusteringTree getTree(int index) {
 		return list.get(index);
+	}
+	
+	public int [] RFPredict (double [] X, int numOfLabels) {
+		int [] p = new int [numOfLabels];
+		for(int i=0;i<this.numberOfTrees;i++) {
+			ClusteringTree t = this.list.get(i);
+			double [][] Y = 
+		}
 	}
 }
